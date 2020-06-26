de8ac52180e2c35392d224ec2d78efce4fe6ace3
Jesse <lavolpeblue@gmail.com>
2020-06-26 20:08:43 -0300

    Acrescentado

--- src/application/ForEach.java
+++ src/application/ForEach.java
@@ -1,10 +1,21 @@
 package application;
 
+import java.util.Scanner;
+
 public class ForEach {
 
 	public static void main(String[] args) {
-		// TODO Auto-generated method stub
-		String[] vetor = new String[] {"Brasil","Itália","Espanha","França"};
+		Scanner sc = new Scanner(System.in);
+		System.out.print("Entre com o número de elementos: ");
+		
+		int n = sc.nextInt();
+		
+		String[] vetor = new String[n] ;
+		System.out.println("Digite os nomes desejados:");
+		for(int i =0;i<n;i++) {
+			vetor[i]=sc.nextLine();
+		}
+		
 		
 		// Para cada ITEM do VETOR 
 		for (String item : vetor) {
